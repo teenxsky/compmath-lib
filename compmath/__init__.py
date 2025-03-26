bde1f2f56c85b1e18cc756e34d8ddc5bdfba19fb
teenxsky <kutorgin2002@gmail.com>
2025-03-27 01:36:57 +1000

    Add methods and classes for calculate math errors

--- compmath/__init__.py
+++ compmath/__init__.py
@@ -1,4 +1,4 @@
-""" CompMath (Computational Mathematics) - Library for complex mathematical calculations
+"""CompMath (Computational Mathematics) - Library for complex mathematical calculations
 
 This library provides tools to perform calculations in the field
 computing mathematics, including numerical methods, linear algebra,
@@ -12,7 +12,19 @@ Basic possibilities:
 - Solution of ordinary differential equations (ode)
 
 Modules:
-- `faults`: a module for calculation faults.
+- `math_errors`: a module for calculation errors.
 """
 
-from .faults import *
+import importlib as _importlib
+
+
+MODULES = [
+    "math_errors",
+]
+
+
+def __getattr__(name):
+    if name in MODULES:
+        return _importlib.import_module(f"compmath.{name}")
+    else:
+        raise AttributeError(f"Module 'compmath' has no attribute '{name}'")
